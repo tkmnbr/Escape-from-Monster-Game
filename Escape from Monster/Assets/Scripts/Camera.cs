c382c7bb1fccd03a50656005859b2ed0fc5a6a60
Noboru Tokimi <t.nbr0304@gmail.com>
2022-06-03 04:26:20 -0700

    add Character movement and camera follow

---
+++ Escape from Monster/Assets/Scripts/Camera.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private string PLAYER_TAG = "Player";
+    private Transform player;
+    private Vector3 tempPos;
+    [SerializeField]
+    private float minX, maxX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag(PLAYER_TAG).transform;
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        tempPos = transform.position;
+        tempPos.x = player.position.x;
+        if (tempPos.x < minX)
+        {
+            tempPos.x = minX;
+        }
+
+        if (tempPos.x > maxX)
+        {
+            tempPos.x = maxX;
+        }
+
+
+        transform.position = tempPos;
+
+        
+    }
+}
---
+++ Escape from Monster/Assets/Scripts/Player.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float moveForce = 10f;
+    [SerializeField]
+    private float jumpForce = 11f;
+    private float movementX;
+    private bool isGrounded;
+    private Rigidbody2D myBody;
+    private SpriteRenderer sr;
+    private Animator anim;
+    private string WALK_ANIMATION = "Walk";
+    private string GROUND_TAG = "Ground";
+
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoveKeyboard();
+        AnimationPlayer();
+        PlayerJump();
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerJump();
+    }
+
+    void PlayerMoveKeyboard()
+    {
+        movementX = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce;
+    }
+
+    void PlayerJump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isGrounded = false;
+            myBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag(GROUND_TAG))
+        {
+            isGrounded = true;
+        }    
+    }
+
+
+    void AnimationPlayer()
+    {
+        if (movementX > 0)
+        {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = false;
+        }
+        else if (movementX < 0)
+        {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = true;
+        }
+        else
+        {
+            anim.SetBool(WALK_ANIMATION, false);
+        }
+    }
+}
